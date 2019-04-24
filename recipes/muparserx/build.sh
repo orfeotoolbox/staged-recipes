a14a4135d9ec7b55e09259388b8ed5cbb66d8cd0
Emmanuelle Sarrazin <emmanuelle.sarrazin@cnes.fr>
2019-04-24 15:56:22 +0000

    BUG: Correct muparserx recipe

--- recipes/muparserx/build.sh
+++ recipes/muparserx/build.sh
@@ -1,26 +1,19 @@
 #!/usr/bin/env bash
 
-#!/usr/bin/env bash
-
 set +x
 set -e # Abort on error
 
 [[ -d build ]] || mkdir build
 cd build
 
-echo "Current work directory: $(pwd)"
-echo "BUILD_PREFIX: ${BUILD_PREFIX}"
-echo "PREFIX: ${PREFIX}"
-
-export CMAKE_LIBRARY_PATH=${BUILD_PREFIX}/lib:${CMAKE_LIBRARY_PATH}
-export CMAKE_INCLUDE_PATH=${BUILD_PREFIX}/include:${CMAKE_INCLUDE_PATH}
-export CMAKE_LIBRARY_PATH=${BUILD_PREFIX}/${HOST}/sysroot/usr/lib:${CMAKE_LIBRARY_PATH}
-
 if [[ $target_platform =~ linux.* ]]; then
     export LDFLAGS="$LDFLAGS -Wl,-rpath-link,${PREFIX}/lib"
 fi
 
 CC=${BUILD_PREFIX}/bin/${HOST}-gcc CXX=$BUILD_PREFIX/bin/${HOST}-g++ \
-    cmake -D CMAKE_INSTALL_PREFIX=$PREFIX -DBUILD_SHARED_LIBS:BOOL=ON ..
+    cmake -G "Ninja" \
+    -DCMAKE_INSTALL_PREFIX=$PREFIX \
+    -DBUILD_SHARED_LIBS:BOOL=ON \
+    -DBUILD_EXAMPLES:BOOL=OFF ..
 ninja install
 
