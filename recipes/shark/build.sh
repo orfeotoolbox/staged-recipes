aebb7d9599c2e4bd5b34cbf29ce81851854c1c5e
Emmanuelle Sarrazin <emmanuelle.sarrazin@cnes.fr>
2019-05-06 09:20:33 +0000

    ENH: Add Shark recipe

---
+++ recipes/shark/build.sh
@@ -0,0 +1,29 @@
+#!/usr/bin/env bash
+
+set +x
+set -e # Abort on error
+
+[[ -d build ]] || mkdir build
+cd build
+
+export CMAKE_LIBRARY_PATH=${BUILD_PREFIX}/lib:${CMAKE_LIBRARY_PATH}
+export CMAKE_INCLUDE_PATH=${BUILD_PREFIX}/include:${CMAKE_INCLUDE_PATH}
+
+if [[ $target_platform =~ linux.* ]]; then
+    export LDFLAGS="$LDFLAGS -Wl,-rpath-link,${PREFIX}/lib"
+fi
+
+CC=${BUILD_PREFIX}/bin/${HOST}-gcc CXX=$BUILD_PREFIX/bin/${HOST}-g++ \
+    cmake -G "Ninja" \
+    -D CMAKE_INSTALL_PREFIX=$PREFIX \
+    -D BUILD_SHARED_LIBS:BOOL=ON \
+    -D BUILD_DOCS:BOOL=OFF \
+    -D BUILD_EXAMPLES:BOOL=OFF \
+    -D BUILD_TESTING:BOOL=OFF \
+    -D ENABLE_HDF5:BOOL=OFF \
+    -D ENABLE_CBLAS:BOOL=OFF \
+    -D ENABLE_OPENMP:BOOL=OFF \
+    ..
+
+ninja install
+
