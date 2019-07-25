c88b1abbb43e0156db56011d0c802aef1c386c3f
David Youssefi <david.youssefi@cnes.fr>
2019-07-25 16:25:32 +0200

    BUG: add vars in activate/deactivate

--- recipes/otb/scripts/activate.sh
+++ recipes/otb/scripts/activate.sh
@@ -6,6 +6,14 @@ if [[ -n "${PYTHONPATH}" ]]; then
     export _CONDA_SET_PYTHONPATH=${PYTHONPATH}
 fi
 
+if [[ -n "${OTB_APPLICATION_PATH}" ]]; then
+    export _CONDA_SET_OTB_APPLICATION_PATH=${OTB_APPLICATION_PATH}
+fi
+
 if [ -d ${CONDA_PREFIX}/lib/otb/python/ ]; then
     export PYTHONPATH=${CONDA_PREFIX}/lib/otb/python/:${_CONDA_SET_PYTHONPATH}
+fi
+
+if [ -d ${CONDA_PREFIX}/lib/otb/applications/ ]; then
+    export OTB_APPLICATION_PATH=${CONDA_PREFIX}/lib/otb/applications/:${_CONDA_SET_OTB_APPLICATION_PATH}
 fi
